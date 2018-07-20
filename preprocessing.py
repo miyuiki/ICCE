baa07fa3fb0e01233cef7983f067ebc5088a0636
miyuiki <a121406@gmail.com>
2018-07-20 11:17:17 +0800

    0720_mac
    Enable input a argument to determine which file wnat to read.

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import csv
+import sys
 
 
 class Student():
@@ -24,7 +25,7 @@ if __name__ == '__main__':
     student_id = []
     student_set = []
     student = []
-    data = pd.read_csv('data1_clickstream.csv', sep=',')
+    data = pd.read_csv('data{}_clickstream.csv'.format(sys.argv[1]), sep=',')
     for row in range(0, data.shape[0]):
         student_id.append(data['userid'][row])
     for i in student_id:
@@ -33,26 +34,29 @@ if __name__ == '__main__':
     for uid in student_set:
         student.append(Student(uid))
 
+
     for s in range(0, len(student_set)):
-        for i in range(0, len(student_id)):
-            if (student[s].name == student_id[i] and data['operationname'][i] == 'PAGE_JUMP'):
-                student[s].PAGE_JUMP += 1
-            elif (student[s].name == student_id[i] and data['operationname'][i] == 'ADD MARKER'):
-                student[s].marker += 1
-            elif student[s].name == student_id[i] and data['operationname'][i] == 'ADD BOOKMARK':
-                student[s].bookmark += 1
-            elif student[s].name == student_id[i] and data['operationname'][i] == 'ADD MEMO':
-                student[s].memo += 1
-            elif student[s].name == student_id[i] and data['operationname'][i] == 'NEXT':
-                student[s].nextc += 1
-            elif (student[s].name == student_id[i] and data['operationname'][i] == 'NEXT'):
-                student[s].prevc += 1
-        student[s].read_page = student[s].nextc - student[s].prevc + 1
-    # for s in range(0,len(student_set)):
-    # 	print("{} PAGE_JUMP:{} MARKERC:{} BOOKMARKC:{} MEMOC:{} read_page:{}"
-    # 		.format(student[s].name,student[s].PAGE_JUMP,student[s].marker
-    # 		,student[s].bookmark,student[s].memo,student[s].read_page))
-    with open('features.csv', 'w', newline='') as csvfile:
+            for i in range(0, len(student_id)):
+                if student[s].name == student_id[i]:
+                    if data['operationname'][i] == 'PAGE_JUMP':
+                        student[s].PAGE_JUMP += 1
+                    elif data['operationname'][i] == 'ADD MARKER':
+                        student[s].marker += 1
+                    elif data['operationname'][i] == 'ADD BOOKMARK':
+                        student[s].bookmark += 1
+                    elif data['operationname'][i] == 'ADD MEMO':
+                        student[s].memo += 1
+                    elif data['operationname'][i] == 'NEXT':
+                        student[s].nextc += 1
+                    elif data['operationname'][i] == 'NEXT':
+                        student[s].prevc += 1
+                    else:
+                        pass
+                else:
+                    pass
+            student[s].read_page = student[s].nextc - student[s].prevc + 1
+
+    with open('features_{}.csv'.format(sys.argv[1]), 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['userid', 'PageJumpC', 'MarkerC', 'bookmarkC', ' MemoC', 'Readpages'])
         for s in range(0, len(student_set)):
